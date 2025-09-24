0db9bd391f9bf5e033b01a8dace886a77b93495b
QuentinRdl <quentinradlo.pro@gmail.com>
2025-09-24 22:28:00 +0200

    [ADD] - Original commit

---
+++ src/main/java/fr/ufrst/m1info/gl/compgl/compgl/Launcher.java
@@ -0,0 +1,9 @@
+package fr.ufrst.m1info.gl.compgl.compgl;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
