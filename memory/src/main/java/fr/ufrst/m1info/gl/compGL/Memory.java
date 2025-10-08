c2b929c6d84191f5e11cfdf707645a46cf1b1692
kjelic <kilian.jelic@edu.univ-fcomte.fr>
2025-10-08 17:51:31 +0200

    [ADD] Added the class that will be used to make the memory API (even tho it's empty)

---
+++ memory/src/main/java/fr/ufrst/m1info/gl/compGL/Memory.java
@@ -0,0 +1,10 @@
+package fr.ufrst.m1info.gl.compGL;
+
+/**
+ * API class for the Memory interface
+ * All methods will be used by the abstract syntax tree for the interpretation/compilation
+ */
+public class Memory {
+
+
+}
