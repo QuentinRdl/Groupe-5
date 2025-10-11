e181dd037302c505e6056613ee41e2ae8c808c02
QuentinRdl <quentinradlo.pro@gmail.com>
2025-10-11 16:28:39 +0200

    [ADD] - New class to represent an variable inside the stack + restarted from scratch the newStack class

---
+++ memory/src/main/java/fr/ufrst/m1info/gl/compGL/Stack_Variable.java
@@ -0,0 +1,41 @@
+package fr.ufrst.m1info.gl.compGL;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Represents an entry of a variable in the stack, with name that contains the actual
+ * name of the variable and scope the scope it has as a suffix
+ */
+public class Stack_Variable {
+    private final String name;
+    private Object value;
+    private final int scope;
+
+    public Stack_Variable(String name, Object value, int scope) {
+        this.name = name;
+        this.value = value;
+        this.scope = scope;
+    }
+
+    public String getName() {
+       return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public int getScope() {
+        return scope;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return name + "_" + scope + "=" + value;
+    }
+}
