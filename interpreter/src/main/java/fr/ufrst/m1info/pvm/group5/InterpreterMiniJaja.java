1af37abd141d83b49be19e1742fe14ba7b098f59
theo humbert <theo.humbert06@gmail.com>
2025-10-16 10:45:17 +0200

    [ADD] Depedencies for module interpreter

--- interpreter/src/main/java/fr/ufrst/m1info/pvm/group5/InterpreterMiniJaja.java
+++ interpreter/src/main/java/fr/ufrst/m1info/pvm/group5/InterpreterMiniJaja.java
@@ -1,4 +1,28 @@
 package fr.ufrst.m1info.pvm.group5;
 
-public class InterpreterMiniJaja {
+
+public class InterpreterMiniJaja implements Interpreter{
+    /**
+     * Interpret the given code
+     *
+     * @param code the code we want to interpret
+     */
+    @Override
+    public void interpretCode(String code) throws Exception {
+        Memory mem = new Memory();
+        AbstractSyntaxTree ast = AbstractSyntaxTree.fromString(code);
+        ast.interpret();
+    }
+
+    /**
+     * Interpret the given file
+     *
+     * @param path the path of the file we want to interpret
+     */
+    @Override
+    public void interpretFile(String path) throws Exception {
+        Memory mem = new Memory();
+        AbstractSyntaxTree ast = AbstractSyntaxTree.fromFile(path);
+        ast.interpret();
+    }
 }
