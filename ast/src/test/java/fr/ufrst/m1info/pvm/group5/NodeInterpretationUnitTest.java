f545e35a242db6f77b0c431a30be319cac5ebb7c
Unity <k.jelic.74@gmail.com>
2025-10-20 15:53:41 +0200

    [TEST] Added more test to test the ADD ast node

--- ast/src/test/java/fr/ufrst/m1info/pvm/group5/NodeInterpretationUnitTest.java
+++ ast/src/test/java/fr/ufrst/m1info/pvm/group5/NodeInterpretationUnitTest.java
@@ -3,16 +3,14 @@ package fr.ufrst.m1info.pvm.group5;
 import fr.ufrst.m1info.pvm.group5.Nodes.ASTNode;
 import fr.ufrst.m1info.pvm.group5.Nodes.AddNode;
 import fr.ufrst.m1info.pvm.group5.Nodes.NumberNode;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
 import org.mockito.Mock;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -22,7 +20,7 @@ public class NodeInterpretationUnitTest {
     @Mock
     Memory memory;
 
-    @BeforeEach
+    @Before
     public void setup(){
         memoryStorage = new HashMap<>();
         memory = ASTMocks.createMemoryWithStorage(memoryStorage);
@@ -32,13 +30,27 @@ public class NodeInterpretationUnitTest {
      * ADD node
      */
     @Test
-    @DisplayName("Add - Default")
     public void AddNodeOperationTest(){
         NumberNode lop = mock();
         when(lop.eval(any(Memory.class))).thenReturn(new Value(5));
         NumberNode rop = mock();
         when(rop.eval(any(Memory.class))).thenReturn(new Value(10));
         AddNode tested = new AddNode(lop,rop);
-        assertEquals(10, rop.eval(memory).valueInt);
+        assertEquals(15, tested.eval(memory).valueInt);
+    }
+
+    @Test(expected = ASTBuildException.class)
+    public void AddNodeMissingOperand(){
+        NumberNode lop = mock();
+        when(lop.eval(any(Memory.class))).thenReturn(new Value(5));
+        AddNode tested = new AddNode(lop,null);
+    }
+
+    @Test(expected = ASTBuildException.class)
+    public void AddNodeInvalidOperand(){
+        ASTNode lop = mock();
+        NumberNode rop = mock();
+        when(rop.eval(any(Memory.class))).thenReturn(new Value(10));
+        AddNode tested = new AddNode(lop,rop);
     }
 }
