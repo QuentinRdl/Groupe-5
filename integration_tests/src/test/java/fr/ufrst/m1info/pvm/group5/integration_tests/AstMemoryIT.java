787546ab7d14039675e389988ed5149eaac0f40b
QuentinRdl <quentinradlo.pro@gmail.com>
2025-10-31 14:35:09 +0100

    [TEST] - Update integration tests for AbstractSyntaxTree to use Value instances and improve memory path handling

--- integration_tests/src/test/java/fr/ufrst/m1info/pvm/group5/integration_tests/AstMemoryIT.java
+++ integration_tests/src/test/java/fr/ufrst/m1info/pvm/group5/integration_tests/AstMemoryIT.java
@@ -1,18 +1,21 @@
 package fr.ufrst.m1info.pvm.group5.integration_tests;
 
+import fr.ufrst.m1info.pvm.group5.ast.ASTInvalidMemoryException;
 import fr.ufrst.m1info.pvm.group5.ast.AbstractSyntaxTree;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import fr.ufrst.m1info.pvm.group5.memory.Memory;
+import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.DataType;
+import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.EntryKind;
 import fr.ufrst.m1info.pvm.group5.memory.Value;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AstMemoryIT {
-
+    private final String testPath = "../ast/";
     Map<String, Value> memoryStorage;
     Memory memory;
 
@@ -20,105 +23,96 @@ public class AstMemoryIT {
     public void setup(){
         memoryStorage = new HashMap<>();
         memory = new Memory();
+        memory.setPreserveAfterInterpret(true);
     }
 
+    // Conversion of AbstractSyntaxTreeTest
 
     @Test
-    @DisplayName("Evaluation - BasicOperations")
+    @DisplayName("AST -> Memory Int Test | Evaluation - BasicOperations")
     public void BasicOperations() throws Exception {
         AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("../ast/src/test/resources/BasicOperations.mjj");
 
         AST.interpret(memory);
-        // assertEquals(8, memoryStorage.get("x").valueInt);
-        assertEquals(8, memory.val("x"));
+        Object obj = memory.val("x");
+        Value val = (Value) obj;
+        assertEquals(8, val.valueInt);
     }
-    /*
 
     @Test
-    @DisplayName("Evaluation - OperationPrevalence")
+    @DisplayName("AST -> Memory Int Test | Evaluation - OperationPrevalence")
     public void OperationPrevalence() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/OperationPrevalence.mjj");
+
+        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile(testPath + "src/test/resources/OperationPrevalence.mjj");
         AST.interpret(memory);
-        assertEquals(17, memoryStorage.get("x").valueInt);
-        assertEquals(17, memoryStorage.get("y").valueInt);
-        assertEquals(-1, memoryStorage.get("z").valueInt);
-        assertEquals(true, memoryStorage.get("w").valueBool);
-        assertEquals(true, memoryStorage.get("v").valueBool);
+
+        Object obj = memory.val("x");
+        Value val = (Value) obj;
+        assertEquals(17, val.valueInt);
+
+        obj = memory.val("y");
+        val = (Value) obj;
+        assertEquals(17, val.valueInt);
+
+        obj = memory.val("z");
+        val = (Value) obj;
+        assertEquals(-1, val.valueInt);
+
+        obj = memory.val("w");
+        val = (Value) obj;
+        assertTrue(val.valueBool);
+
+        obj = memory.val("v");
+        val = (Value) obj;
+        assertTrue(val.valueBool);
     }
 
     @Test
-    @DisplayName("Evaluation - Undefined Variable / sum")
-    public void UndefinedVariableSum() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromString("class C {int y = 10;main {x += y;}}");
-        boolean success = false;
-        try {
-            AST.interpret(memory);
-        }
-        catch (ASTInvalidMemoryException e) {
-            success = true;
-        }
-        assertTrue(success);
-    }
-
-    @Test
-    @DisplayName("Evaluation - Undefined Variable / Inc")
-    public void UndefinedVariableInc() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromString("class C {int y = 10;main {x++;}}");
-        boolean success = false;
-        try {
-            AST.interpret(memory);
-        }
-        catch (ASTInvalidMemoryException e) {
-            success = true;
-        }
-        assertTrue(success);
-    }
-
-    @Test
-    @DisplayName("Evaluation - Undefined Variable / Evaluation")
-    public void UndefinedVariableEval() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromString("class C {int y = 10;main {y = x;}}");
-        boolean success = false;
-        try {
-            AST.interpret(memory);
-        }
-        catch (ASTInvalidMemoryException e) {
-            success = true;
-        }
-        assertTrue(success);
-    }
-
-    @Test
-    @DisplayName("Evaluation - Local Variables")
+    @DisplayName("AST -> Memory Int Test | Evaluation - Local Variables")
     public void LocalVariables() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/LocalVariables.mjj");
+        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile(testPath + "src/test/resources/LocalVariables.mjj");
         try {
             AST.interpret(memory);
         }
         catch (Exception e) {
             fail(e.getMessage());
         }
-        assertEquals(9, memoryStorage.get("x").valueInt);
+
+        Object obj = memory.val("x");
+        Value val = (Value) obj;
+        assertEquals(9, val.valueInt);
     }
 
     @Test
-    @DisplayName("Evaluation - Conditionnals")
+    @DisplayName("AST -> Memory Int Test | Evaluation - Conditionnals")
     public void Conditionnals() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/Conditionals.mjj");
+        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile(testPath + "src/test/resources/Conditionals.mjj");
         try {
             AST.interpret(memory);
         }
         catch (Exception e) {
             fail(e.getMessage());
         }
-        assertEquals(5, memoryStorage.get("a").valueInt);
-        assertEquals(6, memoryStorage.get("b").valueInt);
-        assertEquals(5, memoryStorage.get("c").valueInt);
-        assertEquals(6, memoryStorage.get("v").valueInt);
+
+        Object obj = memory.val("a");
+        Value val = (Value) obj;
+        assertEquals(5, val.valueInt);
+
+        obj = memory.val("b");
+        val = (Value) obj;
+        assertEquals(6, val.valueInt);
+
+        obj = memory.val("c");
+        val = (Value) obj;
+        assertEquals(5, val.valueInt);
+
+        obj = memory.val("v");
+        val = (Value) obj;
+        assertEquals(6, val.valueInt);
     }
 
     @Test
-    @DisplayName("Evaluation - Loops")
+    @DisplayName("AST -> Memory Int Test | Evaluation - Loops")
     public void Loops() throws Exception {
         AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/Loops.mjj");
         try {
@@ -127,8 +121,12 @@ public class AstMemoryIT {
         catch (Exception e) {
             fail(e.getMessage());
         }
-        assertEquals(104, memoryStorage.get("x").valueInt);
+
+        Object obj = memory.val("x");
+        Value val = (Value) obj;
+        assertEquals(104, val.valueInt);
     }
 
-     */
+    // End of conversion of AbstractSyntaxTreeTest
+    // Conversion of AbstractSyntaxTreeTest
 }
