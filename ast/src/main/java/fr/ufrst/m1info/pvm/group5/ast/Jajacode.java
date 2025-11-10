d8d2478e23f5cb382bf191588302050e2d7c11af
Laura <laura.m.jacqueson@edu.univ-fcomte.fr>
2025-11-10 23:52:14 +0100

    [DOC] Added comments for the JajaCode API class

--- ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Jajacode.java
+++ ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Jajacode.java
@@ -14,6 +14,15 @@ import java.util.List;
 public class Jajacode {
 
 
+    /**
+     * Parse a JajaCode input stream and generate a list of instructions.
+     *
+     * @param in input file stream
+     *
+     * @return list of instructions
+     *
+     * @throws ParseCancellationException throws exceptions if the input file is invalid
+     */
     private static List<Instruction> fromCharStream(CharStream in) throws ParseCancellationException {
         JajaCodeLexer lexer = new JajaCodeLexer(in);
         lexer.removeErrorListeners();
@@ -27,12 +36,27 @@ public class Jajacode {
         return parser.eval().instrs;
     }
 
+    /**
+     * Parse a JajaCode source string and generate a list of instructions.
+     *
+     * @param input JajaCode source code to parse
+     *
+     * @return list of parsed instructions
+     */
     public static List<Instruction> fromString(String input){
         CharStream in = CharStreams.fromString(input);
         return fromCharStream(in);
     }
 
-
+    /**
+     * Parse a JajaCode file and generate a list of instructions.
+     *
+     * @param filePath path to an existing file containing JajaCode source code.
+     *
+     * @return list of parsed instructions
+     *
+     * @throws IOException if an error occurs while reading the file
+     */
     public static List<Instruction> fromFile(String filePath) throws IOException {
         CharStream in = CharStreams.fromFileName(filePath);
         return fromCharStream(in);
