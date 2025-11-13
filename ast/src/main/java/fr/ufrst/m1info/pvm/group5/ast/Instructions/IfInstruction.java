191f12bd169cdafce77f51f2acef93c396528b7f
htheogame <theo.humbert06@gmail.com>
2025-11-13 10:47:17 +0100

    [ADD] Add IfInstruction

--- ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/IfInstruction.java
+++ ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/IfInstruction.java
@@ -1,4 +1,24 @@
 package fr.ufrst.m1info.pvm.group5.ast.Instructions;
 
-public class IfInstruction {
+import fr.ufrst.m1info.pvm.group5.memory.Memory;
+import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.EntryKind;
+import fr.ufrst.m1info.pvm.group5.memory.Value;
+import fr.ufrst.m1info.pvm.group5.memory.ValueType;
+
+public class IfInstruction extends Instruction{
+    int addressIf;
+
+    public IfInstruction(int addressIf){
+        this.addressIf = addressIf;
+    }
+
+
+    @Override
+    public int execute(int address, Memory m) {
+        boolean b = ((Value) (m.pop().getValue())).valueBool;
+        if (b){
+            return addressIf;
+        }
+        return address+1;
+    }
 }
