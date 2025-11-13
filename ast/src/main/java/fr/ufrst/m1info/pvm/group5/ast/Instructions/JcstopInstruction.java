b18eaf2ff31d8cd6016ebdce110800fae22b77e5
htheogame <theo.humbert06@gmail.com>
2025-11-13 13:27:37 +0100

    [ADD] Add method execute in JcstopInstruction

--- ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/JcstopInstruction.java
+++ ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/JcstopInstruction.java
@@ -1,4 +1,11 @@
 package fr.ufrst.m1info.pvm.group5.ast.Instructions;
 
-public class JcstopInstruction {
+import fr.ufrst.m1info.pvm.group5.memory.Memory;
+
+public class JcstopInstruction extends Instruction{
+
+    @Override
+    public int execute(int address, Memory m) {
+        return -1;
+    }
 }
