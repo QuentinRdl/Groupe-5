d8efbdb86a907d44b5c1f5c3fd0143cc0333b278
QuentinRdl <quentinradlo.pro@gmail.com>
2025-11-14 15:58:16 +0100

    [TEST] - 1st Integration test for compiler in CompilationIntegrationTest

---
+++ driver/src/test/java/fr/ufrst/m1info/pvm/group5/driver/CompilationIntegrationTest.java
@@ -0,0 +1,92 @@
+package fr.ufrst.m1info.pvm.group5.driver;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import static fr.ufrst.m1info.pvm.group5.driver.MainControllerTest.createTestFile;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(ApplicationExtension.class)
+public class CompilationIntegrationTest extends ApplicationTest {
+
+    private MainController controller;
+
+    @BeforeAll
+    public static void initTempDir(@TempDir Path td) {
+        MainControllerTest.tempDir = td;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/ufrst/m1info/pvm/group5/driver/main-view.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        controller = loader.getController();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    /**
+     * This automates a good part of the Compiler -> IDE tests
+     *
+     * @param filename name of the file to create in the test temp directory
+     * @param content full file content
+     * @return the text currently present in the controller's output TextArea
+     * @throws Exception if fails
+     */
+    private String createFileLoadCompileAndGetConsole(String filename, String content) throws Exception {
+        String[] lines;
+        if (content == null || content.isEmpty()){
+            lines = new String[0];
+        } else {
+            lines = content.split("\\R", -1);
+        }
+
+        File testFile = createTestFile(filename, lines);
+
+        interact(() -> {
+            controller.loadFile(testFile);
+        });
+        WaitForAsyncUtils.waitForFxEvents();
+
+        interact(() -> {
+            controller.onCompileClicked();
+        });
+        Thread.sleep(50);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        return controller.output.getText();
+    }
+
+
+
+
+
+    @Test
+    public void compilatorWorks() throws Exception {
+        String content = String.join("\n",
+                "class C {",
+                "    int x;",
+                "    main {",
+                "        x = 3 + 4;",
+                "        x++;",
+                "    }",
+                "}"
+        );
+
+        String consoleText = createFileLoadCompileAndGetConsole("test.mjj", content);
+        assertTrue(consoleText.contains("[INFO] Compilation successful!"));
+    }
+}
